167c1970ebd8c4eaa10a7ae6b8f23031cb34af61
leobr <leobrionesherrera@gmail.com>
2020-07-22 12:37:14 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post :create, to: 'sessions#create'
+  post :create, to: 'users#create'
+  get '/signup', to: 'users#new', as: 'new'
 end
