1095ae077af2b927bbf30156bec1fd6871e3d8a3
Kamrin <kamrinkennedy@gmail.com>
2020-07-22 14:52:14 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,6 @@ class SessionsController < ApplicationController
         @user = User.new
     end
 
-    # def create
-    #     @user = User.find_by(name: params[:user][:name])
-    #     authenticated = @user.try(:authenticate, params[:user][:password])
-    #     return redirect_to new_session_path unless authenticated
-    #     session[:user_id] = @user.id
-    #     redirect_to users_path
-    # end
-
     def create
         @user = User.find_by(name: params[:user][:name])
         if @user.try(:authenticate, params[:user][:password])
